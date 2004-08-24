040c750bb275f5427daf531709ec05636456377e
metawave <metawave@528a0afb-6b14-0410-a1f4-90c49fd69055>
2004-08-24 08:55:21 +0000

    Program Engine

---
+++ trunk/BattleShip1942/Engine.java
@@ -0,0 +1,23 @@
+/**
+ * @author Adrian Greiler, Marcel Ryser
+ * 
+ * PROJECT: Battleship 1942
+ *
+ * Changelog:
+ * 
+ * 24.08.2004	MR		Erstellung
+ * 
+ */
+public class Engine {
+
+	public static void main(String[] args) {
+	}
+
+	/**
+	 * @param msg
+	 */
+	public void Eventhandler(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+}
