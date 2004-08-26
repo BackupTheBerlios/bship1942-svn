e9452997dbab5864ce3981e6ae72911626e14bf5
mahanja <mahanja@528a0afb-6b14-0410-a1f4-90c49fd69055>
2004-08-26 09:11:30 +0000

    Class to communicate between the players.

---
+++ trunk/BattleShip1942/ch/bship/Net.java
@@ -0,0 +1,74 @@
+/**
+ * @author Adrian Greiler, Marcel Ryser
+ *
+ * PROJECT: Battleship 1942
+ *
+ * Changelog:
+ *
+ * 26.08.2004   AG      Erstellung
+ *
+ */
+
+import java.io.*;
+import java.net.*;
+
+public class Net extends Thread{
+	Engine engine;
+	
+	InputStream 	_in = null;
+	OutputStream 	_out = null;
+	Socket 		_con = null;
+	ServerSocket 	_s = null;
+	String 		_ip;
+
+	public Net(Engine engine){
+		this.engine = engine;	
+	}
+	
+	public void closeAll() {
+		try {
+			if (_in != null)
+				_in.close(); 
+			if (_out != null)
+				_out.close(); 
+			if (_con != null)
+				_con.close(); 
+		} catch (IOException e) {
+			System.err.println (" " + e);
+		}
+	}
+
+	public void run () {
+		try {
+			_s = new ServerSocket (8888);
+    	  	String n = _s.getInetAddress ().toString ();
+     	   	System.out.println ("name = " + n + " " + _s.getLocalPort ());
+		} catch ( Exception e){
+			System.err.println (e);
+		}
+		while (true) {
+			try {
+				_con = _s.accept ();			
+				byte b[]=new byte [2048];
+				int anzahl=_con.getInputStream().read(b);
+				String msg = new String(b, 0, anzahl);
+				engine.Eventhandler(msg);				
+			} catch(Exception e){
+				System.err.println (e);
+			}
+		}     
+	}	 
+		
+	public void send (String msg) {
+		try {			
+			Socket _s= new Socket(_ip, 8888);
+			_s.getOutputStream().write(msg.getBytes());	
+		} catch (IOException e) {
+			System.err.println (e);
+		}	
+	}
+
+	public void setIP (String ip) {
+		_ip = ip;
+	}
+}
